e19f49697f836b75f3019265950dc23ea316c134
tikza008 <tikzaza008@gmail.com>
2020-08-15 14:40:39 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) 
+{
+  float B,H,ANS;
+  printf("Enter B ");
+  scanf("%f",&B);
+  printf("Enter H ");
+  scanf("%f",&H);
+  ANS = B*H*1/2;
+  printf("ANS%0.1f",ANS);
+
+}
